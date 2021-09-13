fd76d645f4efda284af4bc72945e329cb118f733
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-09-13 10:18:53 -0500

    test exercise two

---
+++ PalindromeExercise/WordSmith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var result = word.SequenceEqual(word.Reverse()) ? true : false;
+            return result;
+        }
+    }
+}
---
+++ PalindromeExerciseTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using PalindromeExercise;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("kayak", true)]
+        [InlineData("abba", true)]
+        [InlineData("queen", false)]
+        public void Test1(string word, bool expected)
+        {
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
